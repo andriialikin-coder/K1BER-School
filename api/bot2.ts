25f3c23fc083c17a238ffb08736fa19bdc63888e
Svitanok-HR <andrii.alikin@gmail.com>
2026-06-22 14:31:14 +0300

    Disable bots

--- api/bot2.ts
+++ api/bot2.ts
@@ -1,17 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import bot from '../src/bot2';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
-    console.log("=== ВХОДЯЩИЙ ТРАФИК ОТ TELEGRAM (BOT 2 - SURVEY) ===");
+    console.log("=== ВХОДЯЩИЙ ТРАФИК ОТ TELEGRAM (BOT 2 - SURVEY) (ОТКЛЮЧЕН) ===");
 
-    try {
-        if (req.method === 'POST') {
-            await bot.handleUpdate(req.body);
-            return res.status(200).send('OK');
-        }
-        return res.status(200).send('Бот-анкетолог на связи, жду POST запросы.');
-    } catch (error) {
-        console.error('Критический сбой обработчика bot2:', error);
-        return res.status(500).send('Internal Error');
-    }
+    return res.status(200).send('Bot 2 is offline.');
 };
--- api/index.ts
+++ api/index.ts
@@ -1,18 +1,8 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import bot from '../src/bot';
 
 export default async (req: VercelRequest, res: VercelResponse) => {
     // Этот лог мы ОБЯЗАНЫ увидеть в панели Vercel
-    console.log("=== ВХОДЯЩИЙ ТРАФИК ОТ TELEGRAM ===");
+    console.log("=== ВХОДЯЩИЙ ТРАФИК ОТ TELEGRAM (ОТКЛЮЧЕН) ===");
 
-    try {
-        if (req.method === 'POST') {
-            await bot.handleUpdate(req.body);
-            return res.status(200).send('OK');
-        }
-        return res.status(200).send('Бот на связи, жду POST запросы.');
-    } catch (error) {
-        console.error('Критический сбой обработчика:', error);
-        return res.status(500).send('Internal Error');
-    }
+    return res.status(200).send('Bot is offline.');
 };
